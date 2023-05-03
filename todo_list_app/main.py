f10fabfa8fdae3190ded36a1044feb2aa12c8f91
Zahra <you@example.com>
2023-05-03 09:55:35 +0330

    add get_todos function to main projet

--- todo_list_app/main.py
+++ todo_list_app/main.py
@@ -1,24 +1,26 @@
-user_prompt = "Type add, show, edit, complete, exit:"
+def get_togos():
+    with open("todos.txt", 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
 
-while True :
+
+while True:
+    user_prompt = "Type add, show, edit, complete, exit:"
     user_action = input(user_prompt).strip()
     if user_action.startswith("add"):
-        with open("todos.txt", 'r') as file:
-            todos = file.readlines()
+        todos = get_togos()
         todo = user_action[4:] + "\n"
         todos.append(todo)
         with open("todos.txt", 'w') as file:
             file.writelines(todos)
     elif user_action.startswith("show"):
-        with open("todos.txt", 'r') as file:
-            todos = file.readlines()
+        todos = get_togos()
         for index, item in enumerate(todos):
             print(f"{index+1}-{item[:len(item)-1:]}")
     elif user_action.startswith("edit"):
         try:
             todo_edit_number = int(user_action[5:])-1
-            with open("todos.txt", 'r') as file:
-                todos = file.readlines()
+            todos = get_togos()
             todo_to_edit = todos[todo_edit_number].strip("\n")
             todo_edit = input("Enter value of todo for edit: ") +"\n"
             todos[todo_edit_number] = todo_edit
@@ -34,8 +36,7 @@ while True :
     elif user_action.startswith("complete"):
         try:
             todo_delete_number = int(user_action[9:]) - 1
-            with open("todos.txt", 'r') as file:
-                todos = file.readlines()
+            todos = get_togos()
             todo_to_remove= todos[todo_delete_number].strip("\n")
             todos.pop(todo_delete_number)
             with open("todos.txt", 'w') as file:
