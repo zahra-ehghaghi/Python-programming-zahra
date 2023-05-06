15f9e23cfccb604324d0ef9353200926b9ed9c0e
Zahra <you@example.com>
2023-05-06 17:56:32 +0330

    "Implement the Add button"

--- todo_list_app/gui.py
+++ todo_list_app/gui.py
@@ -3,9 +3,25 @@ import  PySimpleGUI as sg
 
 
 label = sg.Text("Type in a to-do")
-input_box= sg.InputText(tooltip="Enter todo")
-button = sg.Button("Add")
-window = sg.Window("My To-Do App",layout=[[label],[input_box,button]])
+input_box= sg.InputText(tooltip="Enter todo", key="todo")
+add_button = sg.Button("Add")
 
-window.read()
+
+window = sg.Window("My To-Do App",
+                   layout=[[label],[input_box,add_button]],
+                   font=("Helvetica",15))
+
+while True:
+    events, values =window.read()
+    print (events)
+    print (values)
+    match events:
+        case "Add":
+            todos = functions.get_togos()
+            new_todo = values['todo']+"\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
