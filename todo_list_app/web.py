d21ad1ce46726be3d5a09ab06cf1cc65cf260490
Zahra <you@example.com>
2023-05-07 17:54:43 +0330

    "create a static web interface"

---
+++ todo_list_app/web.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from modules import  functions
+
+
+todos = functions.get_togos()
+
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app")
+st.write("This app is to increase your productivity")
+for todo in todos:
+    st.checkbox(todo)
+st.text_input(label="", placeholder="Add new todo.")
