6b8463a614fefdddd4dc9be521a7e51885d1629e
Zahra <you@example.com>
2023-05-15 00:01:52 +0330

    Day 22: render the app titles into 2 columns and add description, image and link to it

--- app2-portfolio/main.py
+++ app2-portfolio/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 with col1:
@@ -17,4 +18,20 @@ content="""
 Below you can fine someof the apps i have built in Python. Feel free to contact me!
 """
 st.write(content)
+col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
+df= pandas.read_csv("data.csv",sep=";")
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"./images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"./images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
 
