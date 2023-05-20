84fe5e4b548b43864223d3b1adbaf13e673d39cd
Zahra <you@example.com>
2023-05-20 08:49:03 +0330

    day24: Exercise: Add lines

--- app3-pdf-template/main.py
+++ app3-pdf-template/main.py
@@ -11,14 +11,22 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"],  ln=1, align="L")
-    pdf.line(x1=10,y1=21,x2=200,y2=21)
+
+    for y in range(20,295,10):
+        pdf.line(10,y,200,y)
+
+
     # set the footer of main page
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=8, txt=row["Topic"], ln=1, align="R")
+
     for i in range (row["Pages"]-1):
         pdf.add_page()
+        for y in range(20, 295, 10):
+            pdf.line(10, y, 200, y)
+
         # set the footer of child page
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
