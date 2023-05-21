a3c9e97f6b18cd6833c4a22c90fa7d53a1df054b
zahra-ehghaghi <z.ehghaghi@gmail.com>
2023-05-21 23:21:38 +0330

    Day26: Student exercise , create a multipage pdf file from multiple text files

--- Students_App2_Text_Files_to_PDF/main.py
+++ Students_App2_Text_Files_to_PDF/main.py
@@ -7,7 +7,10 @@ for file in filepaths:
     title =Path(file).stem
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=8,txt=title.capitalize(),border=0,ln=1)
+    pdf.cell(w=50, h=8, txt=title.capitalize(),border=0,ln=1)
+    with open(file,"r") as filedata:
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.multi_cell(w=0, h=6, txt=str(filedata.read()), border=0)
 pdf.output("output.pdf")
 
 
