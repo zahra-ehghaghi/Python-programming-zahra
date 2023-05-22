c94f60ea5e4f3af4c8213b5364e5ce07ab007fd5
zahra-ehghaghi <z.ehghaghi@gmail.com>
2023-05-22 18:47:16 +0330

    Day27: Send email with titles and descriptions

--- app5-news-api-email/main.py
+++ app5-news-api-email/main.py
@@ -6,7 +6,12 @@ apikey="5aae834347ea42eaa2cc7ff5aa3a196d"
 url=f"https://newsapi.org/v2/everything?q=tesla&from=2023-04-22&sortBy=publishedAt&apiKey={apikey}"
 request = requests.get(url)
 content = request.json()
-
+body=""
 for item in content["articles"]:
- print(item['title'])
- print(item['description'])
+   if item['title'] is not None:
+      body = body + item['title'] +"\n"+item['description'] + 2*"\n"
+
+print(body)
+body= body.encode("utf-8")
+send_email(body)
+print("ok")
