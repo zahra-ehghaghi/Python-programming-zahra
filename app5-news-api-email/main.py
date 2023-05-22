284c456edfbceee26eb7e46357784ad404b1c720
zahra-ehghaghi <z.ehghaghi@gmail.com>
2023-05-22 19:21:04 +0330

    Day28: Add links, specify language, parametrise query, limit to 20  number of result and add subject to email

--- app5-news-api-email/main.py
+++ app5-news-api-email/main.py
@@ -1,15 +1,16 @@
 import requests
 
 from send_email import send_email
-
+keysearch= "tesla"
 apikey="5aae834347ea42eaa2cc7ff5aa3a196d"
-url=f"https://newsapi.org/v2/everything?q=tesla&from=2023-04-22&sortBy=publishedAt&apiKey={apikey}"
+url=f"https://newsapi.org/v2/everything?q={keysearch}&from=2023-04-22&sortBy=publishedAt&apiKey={apikey}&language=en"
 request = requests.get(url)
 content = request.json()
-body=""
-for item in content["articles"]:
-   if item['title'] is not None:
-      body = body + item['title'] +"\n"+item['description'] + 2*"\n"
+
+body="Subject:New NewsLetter Email \n"
+for item in content["articles"][:20]:
+    if item['title'] is not None:
+       body = body + item['title'] +"\n"+item['description'] + "\n" + item['url']+ 2*"\n"
 
 print(body)
 body= body.encode("utf-8")
