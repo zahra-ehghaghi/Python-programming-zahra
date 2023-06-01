8843e3d05a0b864510b78be907ad257ec3b34438
zahra-ehghaghi <z.ehghaghi@gmail.com>
2023-06-01 09:59:46 +0330

    Day 31: Add URL endpoint for yearly and all dates

--- app6-yuor-weather-api/main.py
+++ app6-yuor-weather-api/main.py
@@ -20,5 +20,20 @@ def about(station,date):
     return {"station":station,
             "date": date,
             "temperature":temperature}
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename= "data-small/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename,skiprows=20, parse_dates=["    DATE"])
+    return df.to_dict(orient="records")
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    filename= "data-small/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename,skiprows=20)
+    df["    DATE"]=df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))]
+    return  result.to_dict(orient="records")
+
 if __name__ =="__main__":
     app.run(debug=True)
