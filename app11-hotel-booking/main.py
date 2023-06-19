57ea2cbe9885c9558407147aeb707f7ca04a0518
zahra-ehghaghi <z.ehghaghi@gmail.com>
2023-06-19 13:55:37 +0330

    42: create two new classes to check credit card

--- app11-hotel-booking/main.py
+++ app11-hotel-booking/main.py
@@ -1,6 +1,8 @@
 import  pandas as pd
 
 df = pd.read_csv("hotels.csv",dtype={"id":str})
+df_card = pd.read_csv("cards.csv",dtype=str).to_dict(orient="records")
+df_secure_card = pd.read_csv("card_security.csv",dtype=str)
 
 
 
@@ -38,16 +40,42 @@ class ReservationTicket:
     Hotel Name  {self.hotel_object.name}
 """
     return content
+class CreditCard():
+    def __init__(self,card_number):
+        self.card_number = card_number
+
+    def validate(self,expiration, holder, csv):
+        card = {"number":self.card_number,"expiration":expiration,"cvc":csv,"holder":holder}
+        print(card)
+        if card in df_card:
+            return True
+        else:
+            return False
+
+class SecureCreditCard(CreditCard):
+    def authenticate(self,given_passwoed):
+        password = df_secure_card.loc[df_secure_card["number"]== self.card_number,"password"].squeeze()
+        if password == given_passwoed:
+            return True
+        else:
+            return  False
 
 print(df)
 hotel_ID = input("Enter the id of the hotel:")
 print(hotel_ID)
 hotel = Hotel(hotel_ID)
 if  hotel.available():
-   hotel.book()
-   name = input("Enter your name:")
-   reservation_ticket = ReservationTicket(name,hotel)
-   print(reservation_ticket.generate())
+    creditcard  = SecureCreditCard("1234567890123456")
+    if creditcard.validate(expiration="12/26", holder="JOHN SMITH", csv="123"):
+        if creditcard.authenticate("mypass"):
+            hotel.book()
+            name = input("Enter your name:")
+            reservation_ticket = ReservationTicket(name,hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("CreditCard authentication failed")
+    else:
+        print("CreditCard is not available")
 else:
    print("hotel is not free")
 
