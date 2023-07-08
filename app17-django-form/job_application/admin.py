aa124e0b32cc9187869a9eb746f09c63813e6629
zahra-ehghaghi <z.ehghaghi@gmail.com>
2023-07-08 12:28:44 +0330

    chap54: create a sample django app for submitting application request and create an admin console for it

---
+++ app17-django-form/job_application/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import  Form
+
+class FormAdmin(admin.ModelAdmin):
+    list_display=('first_name','last_name','email','date','occupation')
+    search_fields =('first_name','last_name','email')
+    list_filter = ('date','occupation')
+    ordering = ('-first_name',)
+    readonly_fields = ('occupation',)
+admin.site.register(Form,FormAdmin)
